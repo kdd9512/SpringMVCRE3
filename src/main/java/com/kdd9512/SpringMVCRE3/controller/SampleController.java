77bfb91a3314dd5eac12f632f6abc4017df892fa
kdd9512 <hyunwoo8805@gmail.com>
2022-08-08 23:45:16 +0900

    20220808 1st commit 초기 설정 ** 주의 : project 생성시, Java Enterprise 로 생성할 것. archetype 이 아니다.

---
+++ src/main/java/com/kdd9512/SpringMVCRE3/controller/SampleController.java
@@ -0,0 +1,22 @@
+package com.kdd9512.SpringMVCRE3.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+
+    @GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+    public String getText() {
+
+        log.info("MIME TYPE : " + MediaType.TEXT_PLAIN_VALUE);
+
+        return "SUCCESS";
+    }
+
+}
