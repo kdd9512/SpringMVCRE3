7af999f85d35f48dd2a66ece855b95fdcdace296
kdd9512 <hyunwoo8805@gmail.com>
2022-08-09 23:09:44 +0900

    20220809 2nd commit
    ** SampleController
    GetMapping 을 이용하여 JSON 객체 생성 실습.
    IntStream 을 이용하여 특정 루프만큼 객체 생성 실습.
    추후 삭제예정.
    
    ** SampleVO
    실습을 위한 VO 클래스. 추후 삭제예정.
    
    ?? : application/xml 형식으로 data 가 안보내진다.

--- src/main/java/com/kdd9512/SpringMVCRE3/controller/SampleController.java
+++ src/main/java/com/kdd9512/SpringMVCRE3/controller/SampleController.java
@@ -1,10 +1,15 @@
 package com.kdd9512.SpringMVCRE3.controller;
 
+import com.kdd9512.domain.SampleVO;
 import lombok.extern.log4j.Log4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/sample")
@@ -17,6 +22,23 @@ public class SampleController {
         log.info("MIME TYPE : " + MediaType.TEXT_PLAIN_VALUE);
 
         return "SUCCESS";
+    }
+
+    @GetMapping(value="/getSample",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE }) // xml 은 왜 안되나...
+    public SampleVO getSample() {
+        return new SampleVO(123, "성", "이름");
+    }
+
+    @GetMapping(value="/getList")
+    public List<SampleVO> getList() {
+        // 1~10 미만의 루프를 처리하면서 SampleVO 객체를 생성.
+        return IntStream.range(1,10)
+                .mapToObj(i -> new SampleVO(i, i + " First ", i + " Last ")
+                ).collect(Collectors.toList());
+
     }
 
 }
