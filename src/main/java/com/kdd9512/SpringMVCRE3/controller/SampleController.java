4e7f17452a8c479d5b79a2290ae842e1fabbc10c
kdd9512 <hyunwoo8805@gmail.com>
2022-08-11 21:59:47 +0900

    20220811 4th commit
    ** SampleController
    
    REST 방식으로 호출하기,
    @PathVariable, @RequestBody 실습.
    
    @RequestBody 실습을 위한 새 클래스 Ticket 작성.

--- src/main/java/com/kdd9512/SpringMVCRE3/controller/SampleController.java
+++ src/main/java/com/kdd9512/SpringMVCRE3/controller/SampleController.java
@@ -1,11 +1,12 @@
 package com.kdd9512.SpringMVCRE3.controller;
 
 import com.kdd9512.domain.SampleVO;
+import com.kdd9512.domain.Ticket;
 import lombok.extern.log4j.Log4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -53,5 +54,46 @@ public class SampleController {
         return map;
 
     }
+
+    // REST 방식으로 호출. ResponseEntity 를 이용하여 데이터와 HTTP 헤더의 상태를 함께 전달함.
+    @GetMapping(value = "/check", params={"height" , "weight"})
+    public ResponseEntity<SampleVO> check (Double height, Double weight) {
+
+        SampleVO vo = new SampleVO(0, "" + height, "" + weight);
+
+        ResponseEntity<SampleVO> result = null;
+
+        // height 값이 150보다 작으면 502 error / 이외는 200
+        if (height < 150) {
+            result = ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo);
+        } else {
+            result = ResponseEntity.status(HttpStatus.OK).body(vo);
+        }
+
+        return result;
+    }
+
+    // @PathVariable 실습.
+    // URL 경로 일부를 param 으로 받아 생성할 수 있다.
+    // 이 annotation 을 이용하는 경우 param 값은 기본자료형으로 사용할 수 없다.
+    @GetMapping(value = "/product/{cat}/{pid}")
+    public String[] getPath(
+            @PathVariable("cat") String cat,
+            @PathVariable("pid") String pid) {
+
+        return new String[] {"category : " + cat,", producid : " +  pid};
+    }
+
+    // @RequestBody 실습
+    // 전달된 request 의 내용을 이용하여 해당 param 타입으로 형변환을 요구한다.
+    // 요청받은 내용을 처리하는 방식이므로 GetMapping 이 아니라 PostMapping 을 사용해야한다.
+    @PostMapping("/ticket")
+    public Ticket convert(@RequestBody Ticket ticket) {
+
+        log.info("========================Convert Ticket : " + ticket);
+
+        return ticket;
+    }
+
 
 }
