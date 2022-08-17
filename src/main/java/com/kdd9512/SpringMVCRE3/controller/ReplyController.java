dce5aee28180f0e30055e97f27a19f3b263d834d
kdd9512 <hyunwoo8805@gmail.com>
2022-08-17 22:51:33 +0900

    20220817 5th commit
    Ajax 댓글처리.
    
    CSS 관련 오류로 bootstrap 관련 새 파일 다시가져옴.
    
    ReplyController 에 댓글 조회 / 삭제 기능 구현.
    특정 글이 가지고 있는 댓글의 목록 JSON 으로 화면 출력 실습.

--- src/main/java/com/kdd9512/SpringMVCRE3/controller/ReplyController.java
+++ src/main/java/com/kdd9512/SpringMVCRE3/controller/ReplyController.java
@@ -1,5 +1,6 @@
 package com.kdd9512.SpringMVCRE3.controller;
 
+import com.kdd9512.SpringMVCRE3.domain.Criteria;
 import com.kdd9512.SpringMVCRE3.domain.ReplyVO;
 import com.kdd9512.SpringMVCRE3.service.ReplyService;
 import lombok.AllArgsConstructor;
@@ -7,10 +8,9 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping(value = "/replies/")
 @RestController
@@ -38,6 +38,45 @@ public class ReplyController {
                 new ResponseEntity<>("success", HttpStatus.OK) :
                 new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 
+    }
+
+    @GetMapping(value="/pages/{bno}/{page}",
+            produces = {
+            MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<ReplyVO>> getList(
+            @PathVariable("page") int page, @PathVariable("bno") Long bno) {
+
+        log.info("getList=======================================");
+
+        Criteria cri = new Criteria(page, 10);
+
+        log.info(cri);
+
+        return new ResponseEntity<>(service.getList(cri,bno), HttpStatus.OK);
+
+    }
+
+    // 댓글 조회
+    @GetMapping(value = "/{rno}",
+            produces = {
+            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+
+        log.info("get : " + rno);
+
+        return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+
+    }
+
+    // 댓글 삭제
+    @DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> delete(@PathVariable("rno") Long rno) {
+
+        log.info("Delete : " + rno);
+
+        return service.remove(rno) == 1 ?
+                new ResponseEntity<>("success", HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
